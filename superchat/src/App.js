1d7a49b2440dea60bf70c6ec153bfea6837ef6a7
KERVIN CLARK L. ISIP <isip1800963@ceu.edu.ph>
2021-10-20 11:23:43 +0800

    Minor changes to the code

--- superchat/src/App.js
+++ superchat/src/App.js
@@ -16,9 +16,7 @@ const auth = firebase.auth();
 const firestore = firebase.firestore();
 const analytics = firebase.analytics();
 
-
 function App() {
-
   const [user] = useAuthState(auth);
 
   return (
@@ -37,25 +35,22 @@ function App() {
 }
 
 function SignIn() {
-
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
-  }
+  };
 
   return (
-      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>,
+    <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>,
       <p>Do not violate the community guidelines or you will be banned for life!</p>
-  )
-
+  );
 }
 
 function SignOut() {
   return auth.currentUser && (
     <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
-  )
+  );
 }
-
 
 function ChatRoom() {
   const dummy = useRef();
@@ -66,7 +61,6 @@ function ChatRoom() {
 
   const [formValue, setFormValue] = useState('');
 
-
   const sendMessage = async (e) => {
     e.preventDefault();
 
@@ -76,28 +70,31 @@ function ChatRoom() {
       text: formValue,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       uid,
-      photoURL
-    })
+      photoURL,
+    });
 
     setFormValue('');
     dummy.current.scrollIntoView({ behavior: 'smooth' });
-  }
+  };
 
-  return (<>
-  <main>
+  return (
+    <>
+      <main>
 
-      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+        {messages && messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
 
-      <span ref={dummy}></span>
+        <span ref={dummy}></span>
 
-    </main><form onSubmit={sendMessage}>
+      </main>
+      <form onSubmit={sendMessage}>
 
         <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
 
         <button type="submit" disabled={!formValue}>🕊️</button>
 
       </form>
-  </>)
+    </>
+  );
 }
 
 function ChatMessage(props) {
@@ -105,12 +102,14 @@ function ChatMessage(props) {
 
   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
-  return (<>
-    <div className={`message ${messageClass}`}>
-      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
-      <p>{text}</p>
-    </div>
-  </>)
+  return (
+    <>
+      <div className={`message ${messageClass}`}>
+        <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+        <p>{text}</p>
+      </div>
+    </>
+  );
 }
 
 ChatMessage.propTypes = {
